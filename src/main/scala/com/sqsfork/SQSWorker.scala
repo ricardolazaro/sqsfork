59c17fa605ed4f891de58b384aad1b9d99ce0a84
Ricardo Lazaro de Oliveira <ri.vanlazar@gmail.com>
2013-11-21 11:54:10 -0200

    worker init code

--- src/main/scala/com/sqsfork/SQSWorker.scala
+++ src/main/scala/com/sqsfork/SQSWorker.scala
@@ -1,11 +1,23 @@
 package com.sqsfork
 
 import com.amazonaws.services.sqs.model.Message
+import akka.actor.ActorSystem
+import akka.actor.Props
+import java.util.concurrent.CountDownLatch
 
 trait SQSWorker {
   
   def run(implicit credentials: Credentials, waitForever: Boolean) {
+    val workerName = this.getClass.getSimpleName
+    val system = ActorSystem(workerName)
+    val manager = system.actorOf(Props(new SQSManagerActor(this, credentials)))
     
+    manager ! "bootstrap"
+    
+    if (waitForever) {
+      val job = new CountDownLatch(1)
+      job.await    	
+    }
   }
   
   def config: Map[String, String]
